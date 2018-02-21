3a03d8e3895b2ba19458b0942e61047ba2cdb8d8
Michael Kroth <mkroth65@gmail.com>
2018-02-21 17:13:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,10 @@ function theBeatlesPlay(musicians, instruments){
   
   return strings;
 }
+
+function johnLennonFacts(facts){
+   for(var i = 0; i < facts.length; i++){
+      facts[0] = facts[0] + '!!!';
+   }
+   return facts;
+}
