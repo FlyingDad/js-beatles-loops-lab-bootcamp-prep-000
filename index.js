3ea1f5df4cf5235a0cf340ba1de14d0a47ad8f0a
Michael Kroth <mkroth65@gmail.com>
2018-02-21 17:07:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function johnLennonFacts(musicians, instruments){
   var strings = [];
   
   for(var i = 0; i < musicians.length; i++){
-    
+    strings.push(musicians[i] + ' plays ' + instruments[i]);
   }
   
   return strings;
